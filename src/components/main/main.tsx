09b8d3408a2579c78e19db4580c908dde1a4265e
NEONIX <timuraridi@yandex.ru>
2026-02-02 14:26:05 +0300

    Download guide section

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,12 +2,14 @@ import style from './main.module.css';
 
 import { HeroSection } from './hero-section/heroSection';
 import { BenefitsSection } from './benefits-section/benefitsSection';
+import { DownloadSection } from './download-section/downloadSection';
 
 export const Main = () => {
   return (
     <main className={style.main}>
       <HeroSection />
       <BenefitsSection />
+      <DownloadSection />
     </main>
   )
 }
