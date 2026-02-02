76927a77fb41ab9c7df9883ef3e26701bb10c22d
NEONIX <timuraridi@yandex.ru>
2026-02-02 15:43:38 +0300

    Additional animations

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,16 +5,16 @@ export const Footer = () => {
   
   return (
     <footer className={style.footer}>
-        <svg className={style.logoSvgLeft} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <svg className={`${style.logoSvgLeft} ${style.logoSvg}`} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <title>dotvpn logo</title>
-          <path fill="currentColor" strokeWidth="1" stroke="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8"/>
+          <path className={style.logoSvpPath} fill="currentColor" strokeWidth="1" stroke="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8"/>
         </svg>
         <nav className='style.nav'>
           <a className={style.conf} href="#">:: политика конфиденциальности ::</a>
           <a className={style.logoLink} href="">
             <svg className={style.logoSvg} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <title>dotvpn logo</title>
-              <path fill="currentColor" strokeWidth="1" stroke="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8"/>
+              <path className={style.logoSvpPath} fill="currentColor" strokeWidth="1" stroke="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8s-3.58 8-8 8"/>
             </svg>
           </a>
           <a className={style.rules} href="#">:: правила пользования ::</a>
